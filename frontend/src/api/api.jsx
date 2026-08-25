612bcbce0c90a974141a5384a596795aea3d8b5c
Abhishek Mishra <abhishekmishra2665@gmail.com>
2026-08-25 14:41:37 +0530

    Changeed hardcoded backend api to use env variable

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -13,7 +13,7 @@
 import axios from "axios";
 const api = axios.create({
     headers: {"Context-Type": "application/json"},
-    baseURL: "http://localhost:4000",
+    baseURL: import.meta.env.VITE_API_URL,
 })
 
 export default api;
